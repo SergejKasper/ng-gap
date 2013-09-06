d26630e1419bd4fb1e4360bead063582a0834f06
SergejKasper <sergej.kasper@alice.de>
2013-09-06 23:30:04 +0200

    final

--- build/js/module-google-trends.js
+++ build/js/module-google-trends.js
@@ -3,9 +3,9 @@ angular.module('google-trends', []).directive('googleTrends', [function () {
 			restrict: 'E',
 			link: function (scope, iElement, iAttrs) {
 				scope.$watch(function() {
-	    		return angular.toJson([iAttrs.width, iAttrs.height, iAttrs.url]);
+	    			return angular.toJson([iAttrs.width, iAttrs.height]);
   				}, function() {
-				iElement.html('<iframe width="' + iAttrs.width + '" height="' + iAttrs.height + '" src="' + iAttrs.url + '" style="border: none;"></iframe>');
+					iElement.html('<iframe width="' + iAttrs.width + '" height="' + iAttrs.height + '" src="//www.google.de/trends/fetchComponent?hl\75de\46q\75angularjs+%2B+angular.js,+emberjs+%2B+ember.js,+backbonejs+%2B+backbone.js,+knockoutjs+%2B+knockout.jsr\46date\0751/2011+33m\46cmpt\75q\46content\0751\46cid\75TIMESERIES_GRAPH_0\46export\0755\46w\75800\46h\075600" style="border: none;"></iframe>');
 				});
 			}
 		};
